7d55714b358348c2eff06604d3a017b3a7fb77d9
Trinadh thatakula <trinadh.thatakula@gmail.com>
2025-02-22 09:59:35 +0530

    feat: Update AGP version and enable pending purchases in BillingClient
    This commit updates the Android Gradle Plugin (AGP) version from `8.10.0-alpha05` to `8.10.0-alpha06` in the `libs.versions.toml` file. Additionally, it modifies the `BillingHelper` to enable pending purchases by calling `.enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())` when building the `BillingClient`.

--- app/src/main/java/com/sub/tester/model/BillingHelper.kt
+++ app/src/main/java/com/sub/tester/model/BillingHelper.kt
@@ -8,6 +8,7 @@ import com.android.billingclient.api.BillingClient.BillingResponseCode
 import com.android.billingclient.api.BillingClientStateListener
 import com.android.billingclient.api.BillingFlowParams
 import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.PendingPurchasesParams
 import com.android.billingclient.api.ProductDetails
 import com.android.billingclient.api.Purchase
 import com.android.billingclient.api.PurchaseHistoryRecord
@@ -74,8 +75,8 @@ class BillingHelper(context: Context) {
 
     private var billingClient = BillingClient.newBuilder(context)
         .setListener(purchasesUpdatedListener)
+        .enablePendingPurchases(PendingPurchasesParams.newBuilder().enableOneTimeProducts().build())
         .build()
-
 
     init {
         startConnection {
